eb7d18a42cebf28007565d02a7aef61bc047f810
Pierre-Olivier Trottier <po.trottier@gmail.com>
2021-05-05 15:33:49 -0400

    Update MS Edge to Beta Channel

--- setup.sh
+++ setup.sh
@@ -70,10 +70,10 @@ install_edge() {
 
   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
   sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-  sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+  sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-beta.list'
   sudo rm microsoft.gpg
   sudo apt update
-  sudo apt -y install microsoft-edge-dev
+  sudo apt -y install microsoft-edge-beta
 }
 
 install_gitui() {
